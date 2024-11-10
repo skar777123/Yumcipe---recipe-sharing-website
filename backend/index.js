15912fc26e47da86992734b98193d19e5464c54f
skar777123 <sanskargaikwad206@gmail.com>
2024-11-10 21:26:47 +0530

    changes in backend after deployment

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 dotenv.config();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["https://yumcipe-app.netlify.app/", "http://localhost:3000"],
     credentials: true,
   })
 );
